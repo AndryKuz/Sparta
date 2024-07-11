87bc678b2caf405fc8a77627104ed509e0189970
Андрей Кузьмин <femon1992@gmail.com>
2024-07-11 15:55:50 +0300

    adaptive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import AdditionalServices from "../components/AdditionalServise/AdditionalServices";
 import CartPrograms from "../components/CartPrograms/CartPrograms";
+
 import Offers from "../components/Offers/Offers";
 import OurReviews from "../components/OurReviews/OurReviews";
 
