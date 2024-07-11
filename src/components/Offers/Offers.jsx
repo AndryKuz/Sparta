9bfe77e803ae2541184b5c5a0bb5ce601383462b
Андрей Кузьмин <femon1992@gmail.com>
2024-07-11 09:52:50 +0300

    start map

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -6,15 +6,6 @@ import style from "./Offers.module.scss";
 const Offers = () => {
     return (
         <div className={style.programs}>
-            {/* <div className={style.offers}>
-                <div className={style.title}>
-                    <h3>Our</h3>
-                    <h4>programs</h4>
-                </div>
-                <div>
-                    <span></span>
-                </div>
-            </div> */}
             <Title title1={nameTitle[0][1]} title2={nameTitle[0][2]} />
             <div className={style.buttons}>
                 {nameButton.map(({ title }) => (
--- src/components/OurReviews/OurReviews.jsx
+++ src/components/OurReviews/OurReviews.jsx
@@ -3,13 +3,26 @@ import style from "./OurReviews.module.scss";
 import Title from "../../common/Title";
 import { nameTitle } from "../../constants/common";
 import { Arrow } from "../../common/Arrow";
+import Review from "../Review/Review";
 
 const OurReviews = () => {
     return (
         <div className={style.ourReviews}>
             <div className={style.title}>
                 <Title title1={nameTitle[2][1]} title2={nameTitle[2][2]} />
-                <Arrow direction='left' />
+                <div className={style.navigation}>
+                    <div className={style.wrapperArrow}>
+                        <Arrow direction='left' />
+                        <div className={style.left}></div>
+                    </div>
+                    <div>
+                        <Arrow direction='right' />
+                    </div>
+                </div>
+            </div>
+            <div className={style.reviews}>
+                <Review />
+                <Review />
             </div>
         </div>
     );
